560a2d23f2e16321cea745c26c8d56fc1fa0946a
kndlrs <127057145+kndlrs@users.noreply.github.com>
2023-05-10 16:05:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -10,91 +10,42 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     const {renderer, scene, camera} = mindarThree;
 
-//light is needed when we use 3D objects
-    const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
-    scene.add(light);
+   
+    const video1 = await loadVideo("./assets/videos/Οδηγιες.mov");
+    const texture1 = new THREE.VideoTexture(video1);
+    const geometry1 = new THREE.PlaneGeometry(1, 204/480);
+    const material1 = new THREE.MeshBasicMaterial({map: texture1});
+    const plane1 = new THREE.Mesh(geometry1, material1);
     
-    const video = await loadVideo("./assets/videos/sintel/sintel.mp4");
-    const texture = new THREE.VideoTexture(video);
-    const geometry = new THREE.PlaneGeometry(1, 204/480);
-    const material = new THREE.MeshBasicMaterial({map: texture});
-    const plane = new THREE.Mesh(geometry, material);
-   
-   
-    const raccoon = await loadGLTF('./assets/models/musicband-raccoon/scene.gltf');
-    raccoon.scene.scale.set(0.1, 0.1, 0.1);
-    raccoon.scene.position.set(0, -0.4, 0);
-   
-    
-    const bear = await loadGLTF('./assets/models/musicband-bear/scene.gltf');
-    bear.scene.scale.set(0.1, 0.1, 0.1);
-    bear.scene.position.set(0, -0.4, 0);
+    const anchor1 = mindarThree.addAnchor(0);
+    anchor1.group.add(plane1);
 
-
-
-    //first digital content (3D model with audio)
-    //cooment
-
-    const raccoonAnchor = mindarThree.addAnchor(0);
-    raccoonAnchor.group.add(raccoon.scene);
-
-    const audioClip1 = await loadAudio('./assets/sounds/musicband-background.mp3');
-
-    const listener1 = new THREE.AudioListener();
-    camera.add(listener1);
-
-    const audio1 = new THREE.PositionalAudio(listener1);
-    raccoonAnchor.group.add(audio1);
-
-    audio1.setBuffer(audioClip1);
-    audio1.setRefDistance(100);
-    audio1.setLoop(true);
-
-    raccoonAnchor.onTargetFound = () => {
-      audio1.play();
+    anchor1.onTargetFound = () => {
+      video1.play();
     }
-    raccoonAnchor.onTargetLost = () => {
-      audio1.pause();
+    anchor1.onTargetLost = () => {
+      video1.pause();
     }
-
-    //second digital content (3D model with audio)
-
-    const bearAnchor = mindarThree.addAnchor(1);
-    bearAnchor.group.add(bear.scene);
-
-    const audioClip2 = await loadAudio('./assets/sounds/musicband-background.mp3');
-
-    const listener2 = new THREE.AudioListener();
-    camera.add(listener2);
-
-    const audio2 = new THREE.PositionalAudio(listener2);
-    bearAnchor.group.add(audio2);
-
-    audio2.setBuffer(audioClip2);
-    audio2.setRefDistance(100);
-    audio2.setLoop(true);
-
-    bearAnchor.onTargetFound = () => {
-      audio2.play();
-    }
-    bearAnchor.onTargetLost = () => {
-      audio2.pause();
-    }
-
-    // third digital content (video)
-    const anchor = mindarThree.addAnchor(2);
-    anchor.group.add(plane);
-
-    anchor.onTargetFound = () => {
-      video.play();
-    }
-    anchor.onTargetLost = () => {
-      video.pause();
-    }
-    video.addEventListener( 'play', () => {
-      video.currentTime = 6;
+    video1.addEventListener( 'play', () => {
+      video1.currentTime = 6;
     });
-//start the experience
+     
+   // const video2 = await loadVideo("./assets/videos/Softbox Info.mp4");
+   // const texture2 = new THREE.VideoTexture(video);
+   // const geometry2 = new THREE.PlaneGeometry(1, 204/480);
+   // const material2 = new THREE.MeshBasicMaterial({map: texture});
+   // const plane2 = new THREE.Mesh(geometry, material);
+    
+  //  const video3 = await loadVideo("./assets/videos/BackdropInfo.mov");
+  //  const texture = new THREE.VideoTexture(video);
+  //  const geometry = new THREE.PlaneGeometry(1, 204/480);
+  //  const material = new THREE.MeshBasicMaterial({map: texture});
+  //  const plane = new THREE.Mesh(geometry, material);
+   
+   
+  
+
+    //start the experience
     await mindarThree.start();
     renderer.setAnimationLoop(() => {
       renderer.render(scene, camera);
